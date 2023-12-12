82b7b2c80f4b166ce0becabcd9f666b419dc679e
Krishna <124855697+itshree001@users.noreply.github.com>
2023-12-12 12:32:01 +1100

    Done

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -2,6 +2,11 @@
 
 import ipdb
 
-def plus_two(num):
-    num + 2
+def multiply(num):
+    for index, value in enumerate(num):
+        print(value,index)
+        num[index]=value * 2
     return num
+
+
+multiply([2,4,6])
